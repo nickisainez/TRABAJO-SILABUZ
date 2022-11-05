b7d94e149fcb2e6ac5da74585e78d2f137d9e708
nickisainez <nickiaasainez@gmail.com>
2022-11-05 01:35:24 -0500

    Se creó la clase libros

--- tarea01.py
+++ tarea01.py
@@ -2,27 +2,16 @@
 # Considerar que un libro puede tener varios autores.
 # Se solicita escribir un programa en Python que permita registrar libros.
 
-#Método y función para buscar libro por ISBN
+import pandas as pd
+import csv
 
-listaLib = []
+listaLibros = []
 
 class Libro:
-    def __init__(self,id,titulo,genero,ISBN,editorial,*autores):
+    def __init__(self, id, titulo, genero, isbn, editorial, autor):
         self.id        = id
         self.titulo    = titulo
         self.genero    = genero
-        self.ISBN      = ISBN
+        self.isbn      = isbn
         self.editorial = editorial
-        self.autores   = autores
-    def giveData(self):
-        print(f'ID      TÍTULO   GÉNERO   ISBN   EDITORIAL   AUTOR(ES)')
-        return f'{self.id} - {self.titulo} - {self.genero} - {self.ISBN} - {self.editorial} - {self.autores}'
-  
-    libro_0=[]
-
-def buscarLibro():
-    print("Buscar Libro por el ISBN o Título\n")
-    ISBN = int("Ingrese el numero de cedula a buscar: ")
-    for libro_0 in listaLib:
-        if ISBN == libro_0.isbn:
-            libro_0.giveData()
+        self.autor     = autor
