28608cd1e781297a7a48669de5f60ddb5bf9e043
Daniel <danitec.st@gmail.com>
2022-11-05 13:51:23 -0500

    Se agrego la funcion de eliminar libro y de buscar libros por numero de autores

--- tarea01.py
+++ tarea01.py
@@ -1,7 +1,6 @@
 # Se tiene la clase Libro con los siguientes atributos: id, título, género, ISBN, editorial y autores.
 # Considerar que un libro puede tener varios autores.
 # Se solicita escribir un programa en Python que permita registrar libros.
-
 import pandas as pd
 import csv
 
