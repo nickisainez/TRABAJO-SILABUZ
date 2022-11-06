3aad4591ac20c67373c4f4e18bdca66536e98bc1
maria <cristinabelen97@gmail.com>
2022-11-05 20:08:00 -0500

    Agregando validaciones a los inputs

--- tarea01.py
+++ tarea01.py
@@ -7,7 +7,7 @@ import csv
 listaLibros = []
 
 class Libro:
-    def __init__(self, id, titulo, genero, isbn, editorial, autor):
+    def __init__(self, id:int, titulo:str, genero:str, isbn:int, editorial:str, autor:list):
         self.id        = id
         self.titulo    = titulo
         self.genero    = genero
@@ -28,7 +28,7 @@ class Libro:
     
     def __repr__(self):
         return "{0}:{1}".format(self.id, self.titulo)
-        
+
 ## Zona de validaciones
 def NumberValidation(num)->int:
     while num.isdigit() != True:
@@ -46,21 +46,24 @@ def SpaceValidation(txt_in)->str:
 #######################        
 
 def readFile():
-    path0 = input("Ingrese la dirección relativa del archivo, ejm. (***.csv): ")
-    df = pd.read_csv(path0,header=0)
-    print("LEYENDO TU ARCHIVO CSV")
-    print(df)
-    for i in range(len(df)):
-        info_book =[]
-        for j in df:
-            info = df.loc[i][j]
-            info_book.append(info)
-        book = Libro(info_book[0],info_book[1],info_book[2],info_book[3],info_book[4],info_book[5])
-        listaLibros.append(book)
-        
+    try:
+        path0 = input("Ingrese la dirección relativa del archivo, ejm. (***.csv): ")
+        df = pd.read_csv(path0,header=0)
+        print("LEYENDO TU ARCHIVO CSV 📖")
+        print(df)
+        for i in range(len(df)):
+            info_book =[]
+            for j in df:
+                info = df.loc[i][j]
+                info_book.append(info)
+            book = Libro(info_book[0],info_book[1],info_book[2],info_book[3],info_book[4],info_book[5])
+            listaLibros.append(book)
+    except FileNotFoundError:
+        print('Archivo no encotrado... ')
+        readFile()    
 def registrarLibros():
     autores = []
-    print("Registro de Libro\n")
+    print("Registro de Libro 📙\n")
     while True:
         try:
             id = int(input("Ingrese el id único: "))
@@ -68,8 +71,8 @@ def registrarLibros():
         except ValueError:
             print('Ingrese un entero por favor')
             continue
-    titulo    = input("Ingrese el título: ")
-    genero    = input("Ingrese el género: ")
+    titulo    = SpaceValidation ( input ("Ingrese el título: "))
+    genero    = SpaceValidation ( input ("Ingrese el género: "))
     while True:
         try:
             isbn = int(input("Ingrese el ISBN (13 dígitos): "))
@@ -77,63 +80,64 @@ def registrarLibros():
         except ValueError:
             print('Ingrese correctamente el ISBN por favor')
             continue
-    editorial = input("Ingrese la editorial: ")
-    n_autor = int(input('Cantidad de autores: '))
+    editorial = SpaceValidation  ( input ("Ingrese la editorial: "))
+    n_autor   = NumberValidation ( input ('Cantidad de autores: ')) 
     for i in range(n_autor):
-        autor = input(f'Ingrese Autor {i+1}: ')
+        autor = SpaceValidation ( input (f'Ingrese Autor {i+1}: ')) 
         autores.append(autor)
 
     libro0 = Libro(id, titulo, genero, isbn, editorial, autores)
     listaLibros.append(libro0)
   
 def listadoLibros():
-    print("Listado de Libros\n")
+    print("Listado de Libros 📔\n")
     print("ID | TÍTULO | GÉNERO | ISBN | EDITORIAL | AUTOR(ES)")
     for libro0 in listaLibros:
         libro0.entregarDatos()
 def buscarLibros1():
-    print("Buscar Libro\n")
+    print("Buscar Libro 🔎\n")
     autor = input("Ingrese el autor a buscar: ")
     for libro0 in listaLibros:
         if autor in libro0.autor:
             libro0.entregarDatos()
 
 def buscarLibros2():
-    print("Buscar Libro\n")
+    print("Buscar Libro 🔎\n")
     editorial = input("Ingrese la editorial a buscar: ")
     for libro0 in listaLibros:
         if editorial == libro0.editorial:
             libro0.entregarDatos()
 
 def buscarLibros3():
-    print("Buscar Libro\n")
+    print("Buscar Libro 🔎\n")
     genero = input("Ingrese el genero a buscar: ")
     for libro0 in listaLibros:
         if genero == libro0.genero:
-            libro0.entregarDatos()        
+            libro0.entregarDatos() 
 
 def buscarLibros4():
-    print("Buscar Libro\n")
+    print("Buscar Libro 🔎\n")
     isbn = int(input("Ingrese el ISBN a buscar: "))
     for libro0 in listaLibros:
         if isbn == libro0.isbn:
             libro0.entregarDatos()
-
+           
 def buscarLibros5():
-    print("Buscar Libro\n")
+    print("Buscar Libro 🔎\n")
     titulo = input("Ingrese el título a buscar: ")
     for libro0 in listaLibros:
         if titulo in libro0.titulo:
             libro0.entregarDatos()
+            
 
 def ordenarLibros():
-    print('Ordenar Libros por Titulo')
+    print('Ordenar Libros por Titulo 📚')
     ordenar = sorted(listaLibros, key=lambda x: x.titulo)
     print(ordenar)
 
 def buscar_n_autores():
-    print("Buscar Libro\n")
-    numero = int(input("Ingrese Numero de autores: "))
+    print("Buscar Libro 🔎\n")
+    numero = NumberValidation(input("Ingrese Numero de autores: "))
     for libro0 in listaLibros:
         autores = libro0.autor
         #Limpiamos la cadena recibida por la clase y lo convertimos en una lista para contabilizar los autores
@@ -147,52 +151,62 @@ def buscar_n_autores():
             libro0.entregarDatos()
 
 def modificarLibros():
-    print("Modificar Libro\n")
-    isbn = int(input("Ingrese el numero de ISBN a buscar: "))
+    print("Modificar Libro🔁\n")
+    isbn = NumberValidation(input("Ingrese el numero de ISBN a buscar: "))
     autores =[]
     for libro0 in listaLibros:
         if isbn == libro0.isbn:
-          id        = int(input("Ingrese el id único: "))
-          titulo    = input("Ingrese el título: ")
-          genero    = input("Ingrese el género: ")
-          isbn      = int(input("Ingrese el ISBN: "))
-          editorial = input("Ingrese la editorial: ")
-          n_autor = int(input('Cantidad de autores: '))
+          id        = NumberValidation(input("Ingrese el id único: "))
+          titulo    = SpaceValidation(input("Ingrese el título: "))
+          genero    = SpaceValidation(input("Ingrese el género: "))
+        #   isbn      = int(input("Ingrese el ISBN: "))
+          editorial = SpaceValidation(input("Ingrese la editorial: "))
+          n_autor = NumberValidation(input('Cantidad de autores: '))
           for i in range(n_autor):
-            autor = input(f'Ingrese Autor {i+1}: ')
+            autor = SpaceValidation(input(f'Ingrese Autor {i+1}: '))
             autores.append(autor)
           libro0.editarLibros(id, titulo, genero, isbn, editorial, autores)
           libro0.entregarDatos()
+          break
+    else:    
+        print('Libro no encontrado')
           
 def deleteFile():
-    iddelete = input('Ingrese el ID del libro que desea eliminar: ')
+    print("Eliminar Libro 🗑\n")
+    iddelete = NumberValidation ( input('Ingrese el ID del libro que desea eliminar: '))
     for libro0 in listaLibros:
         if iddelete == libro0.id:
             libro0.entregarDatos()
             listaLibros.remove(libro0)
             print('Eliminado correctamente...')
-
+            break
+    else:    
+        print('Libro no encotrado')
 def guardar_csv():
-    list_save_book = []
-    file = open("books.csv", "w")
-    file.close()
-    atributos = [['ID', 'Titulo', 'Genero', 'ISBN', 'Editorial', 'Autores']]
-    with open('books.csv','a',newline='') as file:
-        write = csv.writer(file, delimiter=',')
-        write.writerows(atributos)
-    for libro in listaLibros:
-        save_book = []
-        save_book.append(libro.id)
-        save_book.append(libro.titulo)
-        save_book.append(libro.genero)
-        save_book.append(libro.isbn)
-        save_book.append(libro.editorial)
-        save_book.append(libro.autor)
-        list_save_book.append(save_book)
-    with open('books.csv','a',newline='') as file:
+    try:
+        print('Guardar en CSV 💾\n')
+        list_save_book = []
+        file = open("books.csv", "w")
+        file.close()
+        atributos = [['ID', 'Titulo', 'Genero', 'ISBN', 'Editorial', 'Autores']]
+        with open('books.csv','a',newline='') as file:
             write = csv.writer(file, delimiter=',')
-            write.writerows(list_save_book)
-    print('Guardado Correctamente')
+            write.writerows(atributos)
+        for libro in listaLibros:
+            save_book = []
+            save_book.append(libro.id)
+            save_book.append(libro.titulo)
+            save_book.append(libro.genero)
+            save_book.append(libro.isbn)
+            save_book.append(libro.editorial)
+            save_book.append(libro.autor)
+            list_save_book.append(save_book)
+        with open('books.csv','a',newline='') as file:
+                write = csv.writer(file, delimiter=',')
+                write.writerows(list_save_book)
+        print('Guardado Correctamente')
+    except csv.Error as e:
+        print('Ocurrio un error', e)    
 def main():
     while True:
         print("\n")
@@ -214,10 +228,11 @@ def main():
         print("10.- Ordenar Libros por Titulo")
         print("11.- Buscar por número de autores")
         print("12.- Guardar Cambios en el CSV")
+        print("0.- Salir")
         
         while True:
             try:
-                opcion = int(input("Opcion: "))
+                opcion = int(input("Opción: "))
                 break
             except ValueError:
                 print("Ingrese una opción válida: ")
@@ -247,7 +262,9 @@ def main():
             buscar_n_autores()
         elif opcion == 12:
             guardar_csv()
-                
+        elif opcion == 0:
+            print('Vuelva pronto 😊 ')
+            quit()  
 if __name__ == '__main__':
     readFile()
     main()    
